63ad5d740e179f8d83d85947bcfd733101250481
gabriel-santana <gabrielsantana77@hotmail.com>
2022-10-24 22:54:24 -0300

    Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { ThemeProvider } from "styled-components";
+
 import { Button } from "./components/Button";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant='primary' />
       <Button variant='secundary'/>
       <Button variant='success'/>
       <Button variant='danger'/>
       <Button />
-    </>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
